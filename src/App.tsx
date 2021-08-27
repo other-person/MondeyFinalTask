eb1198b529b49d424d1f100ff788dd52f971a0c7
Tarik <bazhenkataras@gmail.com>
2021-08-27 21:40:44 +0300

    Add function, state for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Counter from "./Counter/Counter";
+import {Counter} from "./Counter/Counter";
 
 
-function App() {
-  return (
-    <div className="App">
 
-      <Counter/>
+function App () {
+let [display, setDisplay] = useState(0)
 
-    </div>
-  );
+function addNumber () {
+    if (display === 5) {return}
+    setDisplay(display + 1 )
+    };
+
+
+function resetNumber () {
+    setDisplay( 0)
+    };
+
+
+    return (
+        <div className="App">
+
+            <Counter display={display}
+                     addNumber={addNumber}
+                     resetNumber={resetNumber}/>
+
+        </div>
+    );
 }
 export default App;
--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,24 +1,38 @@
 import React from "react";
 import s from "./Counter.module.css"
 
-type CounterPropsType = { props: any }
+type CounterPropsType = {
+    display: number | string
+    addNumber: ()=> void
+    resetNumber: ()=> void
 
-function Counter(props: any) {
+};
 
+export function Counter  ( props: CounterPropsType) {
+
+let last_number = {
+    color: props.display === 5 ? "#ce1515" : ""
+}
+let button_inc = {
+    backgroundColor: props.display < 5 ? '#06f8f8' : ""
+}
+let button_res = {
+    backgroundColor: props.display > 0 ? '#06f8f8' : ""
+}
 
 
     return (
         <section>
             <div className={s.wrapper}>
-                <input className={s.numbers_window} type="text" value={0}/>
+                <div className={s.numbers_window}><div style = {last_number}>{props.display}</div></div>
+
                 <div className={s.wrapper_buttons}>
-                    <button onClick={()=>{}} className={s.button_inc}><p>Inc</p></button>
-                    <button onClick={()=>{}} className={s.button_reset}><p>Reset</p></button>
+                    <button  onClick={()=>{props.addNumber()}} style = {button_inc} className={s.button_inc}><p>Inc</p></button>
+                    <button onClick={()=>{props.resetNumber()}} style = {button_res} className={s.button_reset}><p>Reset</p></button>
                 </div>
             </div>
+
         </section>
 
     )
-}
-
-export default Counter;
+};
